c602d95861fe45bbbe129847263f6711d40012ae
James Peñafiel L <jamespl2009@hotmail.com>
2026-05-04 00:19:16 -0500

    prueba

--- pruebas.py
+++ pruebas.py
@@ -1,8 +1,28 @@
 # prueba.py
 
 from geometria import area_circulo, area_rectangulo, area_triangulo
-
+import matplotlib.pyplot as plt
 
 print("Área del círculo:", area_circulo(1))
 print("Área del rectángulo:", area_rectangulo(4, 5))
 print("Área del triángulo:", area_triangulo(4, 5))
+
+
+
+
+
+
+radios = [1, 2, 3, 4, 5]
+areas = []
+
+for radio in radios:
+    area = area_circulo(radio)
+    areas.append(area)
+
+plt.plot(radios, areas, marker="o")
+plt.xlabel("Radio")
+plt.ylabel("Área")
+plt.title("Área de un círculo")
+plt.grid(True)
+
+plt.show()
